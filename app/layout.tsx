7041ace6e93c70ce7c07933b87e89a486e0e7adc
samuel-eke <eke.eke.eth@gmail.com>
2025-01-08 15:59:32 +0100

    fixed bento grid sizing issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+import { Poppins } from 'next/font/google';
+
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"; //this themeprovider is used to manage the modes of the application from light to dark n vis-versa
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={` ${poppins.variable} antialiased font-poppins`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
-import { index } from "@/data";
+import { gridItems } from "@/data";
 
 function Grid() {
-  return <div>Grid</div>;
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            img={item.img}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 }
 
 export default Grid;
